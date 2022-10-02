b00089319b76bf17822e6e1eadf9451e3c8d57a9
Ugochukwu Nwaufa <50299676+ugonwaufa@users.noreply.github.com>
2022-10-02 00:57:48 -0700

    First Version
    Script models gears procedurally.

---
+++ GearCreator.py
@@ -0,0 +1,40 @@
+from maya import cmds
+
+def createGear(teeth=10, length=0.3):
+    """
+    This function will create a gear with the given parameters
+    :param teeth: The number of teeth to create
+    :param length: The length of the teeth
+    :return:
+        A tuple of the transform, constructor and extrude node
+    """
+    # Teeth are every alternate face, so spans x 2
+    spans = teeth * 2
+
+    transform, constructor = cmds.polyPipe(subdivisionsAxis=spans)
+
+    sideFaces = range(spans*2, spans*3, 2)
+
+    cmds.select(clear=True)
+
+    for face in sideFaces:
+        cmds.select('%s.f[%s]' % (transform, face), add=True)
+
+    extrude = cmds.polyExtrudeFacet(localTranslateZ=length)[0]
+    return transform, constructor, extrude
+
+def changeTeeth(constructor, extrude, teeth=10, length=0.3):
+    spans = teeth*2
+
+    cmds.polyPipe(constructor, edit=True, subdivisionAxis=spans)
+
+    sideFaces = range(spans*2, spans*3, 2)
+    faceNames = []
+
+    for face in sideFaces:
+        faceName = 'f[%s]' % (face)
+        faceNames.append(faceName)
+
+    cmds.setAttr('%s.inputComponents' % (extrude), len(faceNames)
+                 , *faceNames, type="componentList")
+    cmds.polyExtrude(extrude, edit=True, ltz=length)
